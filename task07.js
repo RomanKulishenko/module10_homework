9a537f45bb5077c6988c7373388025266bdf6e8e
RomanKulishenko <romakulishenko@gmail.com>
2023-09-07 18:31:49 +0300

    task07

--- task07.js
+++ task07.js
@@ -4,4 +4,25 @@
 
 // При выполнении задания необходимо учесть, что массив может содержать не только числа, но и, например, знаки, null и так далее.
 
-// не знаю как это сделать
+const arr = [3, 0, 1, 2, 7, 9, 8, 4, 5, 152, null, 11, 6, 0];
+
+let item = 0;
+let itemOdd = 0;
+let itemEven = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 0) {
+    item++;
+  }
+  if (typeof arr[i] === "number") {
+    if (arr[i] % 2) {
+      itemOdd++;
+    } else {
+      itemEven++;
+    }
+  }
+}
+
+console.log("Количество нулей: " + item);
+console.log("Количество нечетных чисел: " + itemOdd);
+console.log("Количество четных чисел: " + itemEven);
